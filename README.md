6c705f48df5b52b9a16f2e5d2371535e6d8a8b45
Pavlo Shylan <crabulik@gmail.com>
2022-05-26 09:56:11 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # tony-calculator
+
+The tool for Tony to add some automation to his buisness.
+
+## Prerequisites
+
+- Python 3+ installed: https://www.python.org/downloads/
+= Tesseract=ocr installed: https://github.com/UB-Mannheim/tesseract/wiki
+
+## How to build
+
+Install dependencies. Run it for several times if there are errors due to missning files.
+
+```
+pip install -r requirements.txt
+```
+
+or use ```runBuild.bat``` file.
+
+## How to run
+
+Go to the ```/config``` directory and set the position of panels with values in the ```cfg.json``` file (where x1,y1 - top left corner, x2,y2 - bottom right corner).
+
+Then run the script
+```
+python calculate.py
+```
+
+or use ```runCalculation.bat``` file.
+
+The captured images can be checked in the ```/tmp``` folder.
+
+## Usefull tools
+
+Mouse Position Tracker: https://github.com/Bluegrams/MPos
---
+++ calculate.py
@@ -0,0 +1,108 @@
+import time
+import numpy as np
+from PIL import ImageGrab
+import cv2
+import os
+import pytesseract
+import json
+from types import SimpleNamespace
+from decimal import Decimal
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+if not os.path.exists("tmp"):
+    os.mkdir("tmp")
+
+def parseValue(image, do_color_invert, value_name, full_log):
+    if full_log :
+        print(f'Parsing {value_name}')
+    np_array = np.array(image)
+    gray = cv2.cvtColor(np_array, cv2.COLOR_BGR2GRAY)
+
+    if do_color_invert :
+        processed = (255-gray)
+    else:
+        processed = gray
+
+    cv2.imwrite(f'tmp/{value_name}.png', processed)
+
+    textValue = pytesseract.image_to_string(processed, lang='eng', config='--psm 6').strip()
+
+    try:
+        return Decimal(textValue)
+    except:
+        raise Exception(f'Cannot parse {value_name}. See the temp directory to verify the captured area.')
+
+
+print("Calculating: X = A * 1000 ")
+print("Calculating: Y = (A * 1000) - (X * B)")
+
+with open("config/cfg.json", "r") as jsonfile:
+    cfg = json.load(jsonfile, object_hook=lambda d: SimpleNamespace(**d))
+    print("Config loaded.")
+print(cfg)
+
+while True :
+    try:
+        if cfg.fullLog :
+            print("Capturing screenshots ...")
+        imageA = ImageGrab.grab(bbox=(cfg.valueA.x1, cfg.valueA.y1, cfg.valueA.x2, cfg.valueA.y2), all_screens=True)
+        imageB = ImageGrab.grab(bbox=(cfg.valueB.x1, cfg.valueB.y1, cfg.valueB.x2, cfg.valueB.y2), all_screens=True)
+
+        if cfg.fullLog :
+            print("Parsing values ...")
+        valA = parseValue(imageA, cfg.valueA.doColorInvert, "ValueA", cfg.fullLog)
+        valB = parseValue(imageB, cfg.valueB.doColorInvert, "ValueB", cfg.fullLog)
+
+        x = valA * 1000
+        y = valA * 1000 + valB * x
+        print(f'X = {x}  Y = {y}')
+    except Exception as err:
+        print(f"{err=}")
+        
+    time.sleep(cfg.delaySeconds)
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+filename = 'Image.png'
+x = 1
+last_time = time.time()
+ 
+while(True):
+    screen =  np.array(ImageGrab.grab(bbox=(581, 708, 992, 771)))
+    print('loop took {} seconds'.format(time.time()-last_time))
+    last_time = time.time()
+    cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    cv2.imwrite(filename, screen)
+    x = x + 1
+    print(x)
+    if x == 2:
+      cv2.destroyAllWindows()
+      break
+ 
+img = cv2.imread('Image.png')
+text = pytesseract.image_to_string(img)
+print(text)
+ 
+index = text.find("You")
+print(index)
+ 
+if index == -1:
+    print("ТАКОГО СЛОВА НЕТЬ!!!")
+else:
+    print("Я НАШЕЛЬ ЕНТО СЛОВО!!!")
+"""
